d4163f6a9efc4f4e3dc52930025da25789dfc611
Mouginot B <mouginot.baptiste@gmail.com>
2018-05-02 15:52:00 -0500

    new files

--- add_variable.cxx
+++ add_variable.cxx
@@ -1,31 +1,55 @@
-{
-  TFile file("systematic/old/tree.root", "update");
-  TTree* in_tree = nullptr;
-  file.GetObject("myTTree", in_tree);
-  Long64_t nentries =
-      in_tree->GetEntries();  // read the number of entries in the t3
-  
+void add_cumul_variable(string file, string var) {
+  TFile *myfile = TFile::Open(file.c_str(), "update");
+  if (!myfile || myfile->IsZombie()) { 
+    cout <<"The file could not be opened!"; 
+    delete myfile;
+    return;
+  }  
+  TTree* in_tree = (TTree*)myfile->Get("myTTree"); 
+
+  string new_branch_name = var + "_cumul";
+  if (in_tree->GetBranch(new_branch_name.c_str())) {
+    std::cout << file << ": Already processed for variable " << var << " !"
+              << std::endl;
+    return;
+  }
+  //std::cout << "Adding " << var << " Cumul in " << file << std::endl;
   vector<double> my_cumul_var;
   vector<double> *pMy_cumul_var = &my_cumul_var;
-  TBranch *cumul_branch = in_tree->Branch("B1_cumul", "std::vector<double>", &pMy_cumul_var);
+  TBranch *cumul_branch = in_tree->Branch(
+      new_branch_name.c_str(), "std::vector<double>", &pMy_cumul_var);
 
-
-
-  TTree* loop_tree = in_tree->CloneTree();
+  TTree *loop_tree = in_tree->CloneTree();
 
   vector<double> *my_var;
-  loop_tree->SetBranchAddress("B1", &my_var);
+  loop_tree->SetBranchAddress(var.c_str(), &my_var);
+  Long64_t nentries =
+      in_tree->GetEntries();  // read the number of entries in the t3
+  std::cout << nentries << endl;
 
-  for (int i = 0; i < nentries; i++){
+  for (int i = 0; i < nentries; i++) {
     loop_tree->GetEntry(i);
     pMy_cumul_var->clear();
     pMy_cumul_var->reserve(my_var->size());
+    std::cout << i << endl;
     double my_cumul = 0;
-    for(int j = 0; j < my_var->size(); j++){
+    for (int j = 0; j < my_var->size(); j++) {
       my_cumul += (*my_var)[i];
       pMy_cumul_var->push_back(my_cumul);
     }
+    std::cout << i << endl;
     cumul_branch->Fill();
-  } 
+  }
   in_tree->Write("", TObject::kOverwrite);
+  myfile->Close("R");
+  
+}
+
+void add_variable() {
+  add_cumul_variable("systematic/old/tree.root", "B2");
+  string file__[6] = {"cooling", "burnup", "cycle", "assay", "power", "separation"};
+  for(int i = 0 ; i < 6; i++){
+    string filename = "systematic_" + file__[i] + "/tree.root";
+    add_cumul_variable(filename ,"B2");
+  }
 }
