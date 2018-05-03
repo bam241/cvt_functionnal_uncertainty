3a7ca966b8ed9e607a01e0ab33b7191eff1e0b73
Mouginot B <mouginot.baptiste@gmail.com>
2018-05-02 19:47:01 -0500

    new soft

--- add_variable.cxx
+++ add_variable.cxx
@@ -1,19 +1,19 @@
 void add_cumul_variable(string file, string var) {
+  std::cout << file << endl;
   TFile *myfile = TFile::Open(file.c_str(), "update");
   if (!myfile || myfile->IsZombie()) { 
     cout <<"The file could not be opened!"; 
     delete myfile;
     return;
   }  
+  std::cout << __LINE__ << endl;
   TTree* in_tree = (TTree*)myfile->Get("myTTree"); 
-
   string new_branch_name = var + "_cumul";
   if (in_tree->GetBranch(new_branch_name.c_str())) {
     std::cout << file << ": Already processed for variable " << var << " !"
               << std::endl;
     return;
   }
-  //std::cout << "Adding " << var << " Cumul in " << file << std::endl;
   vector<double> my_cumul_var;
   vector<double> *pMy_cumul_var = &my_cumul_var;
   TBranch *cumul_branch = in_tree->Branch(
@@ -25,29 +25,32 @@ void add_cumul_variable(string file, string var) {
   loop_tree->SetBranchAddress(var.c_str(), &my_var);
   Long64_t nentries =
       in_tree->GetEntries();  // read the number of entries in the t3
-  std::cout << nentries << endl;
 
   for (int i = 0; i < nentries; i++) {
     loop_tree->GetEntry(i);
+  std::cout << __LINE__ << endl;
     pMy_cumul_var->clear();
     pMy_cumul_var->reserve(my_var->size());
-    std::cout << i << endl;
     double my_cumul = 0;
+  std::cout << __LINE__ << endl;
     for (int j = 0; j < my_var->size(); j++) {
-      my_cumul += (*my_var)[i];
+      my_cumul += (*my_var)[j];
+      
+  std::cout << __LINE__ << endl;
       pMy_cumul_var->push_back(my_cumul);
     }
-    std::cout << i << endl;
     cumul_branch->Fill();
+  std::cout << __LINE__ << endl;
   }
   in_tree->Write("", TObject::kOverwrite);
-  myfile->Close("R");
-  
+  std::cout << __LINE__ << endl;
+ delete myfile; 
 }
 
 void add_variable() {
-  add_cumul_variable("systematic/tree.root", "B1");
-  add_cumul_variable("ref/tree.root", "B1");
+  add_cumul_variable("systematic/old/tree.root", "B1");
+  //add_cumul_variable("systematic_no_cooling_no_cycle/tree.root", "B1");
+  //add_cumul_variable("ref/tree.root", "B1");
   string file__[6] = {"cooling", "burnup", "cycle", "assay", "power", "separation"};
   for(int i = 0 ; i < 6; i++){
     string filename = "systematic_" + file__[i] + "/tree.root";
--- nice_plot.cxx
+++ nice_plot.cxx
@@ -1,7 +1,9 @@
+map<string, string> legend;
 map<string, double> p_max;
 map<string, double> err_max;
 int time_max = 180;
-
+string ref_file = "systematic/old/tree.root";
+//string ref_file = "systematic_no_cooling_no_cycle/tree.root";
 string file[6] = {"cooling", "burnup", "cycle", "assay", "power", "separation"};
 int color[6] = {4, 2, 8, 9, 48, 38};
 
@@ -66,59 +68,86 @@ TGraph* add_rel_uncertainty(string name, string param, TCanvas* C, int color_) {
   // gr_cool->GetYaxis()->SetRangeUser(0,200);
   gr_->SetTitle(name.c_str());
   gr_->SetLineColor(color_);
-  C->cd(2);
+  C->cd();
   gr_->Draw("same");
   return gr_;
 }
 
 void my_plot(string param) {
   TCanvas* C1 = new TCanvas();
-  C1->Divide(1, 2);
+  //C1->Divide(1, 2);
 
-  C1->cd(1);
+  C1->cd();
   TGraphErrors* gr_err =
-      GetOneSigmaDistribution("systematic/old/tree.root", param);
+      GetOneSigmaDistribution(ref_file.c_str(), param);
   gr_err->GetXaxis()->SetRangeUser(0, time_max);
+  gr_err ->SetTitle("Error at 1 #sigma");
   gr_err->SetFillColor(4);
   gr_err->SetFillStyle(3002);
+  gr_err->GetHistogram()->GetXaxis()->SetTitle("Time [y]");
+  gr_err->GetHistogram()->GetYaxis()->SetTitle(legend[param].c_str());
+  gr_err->GetHistogram()->GetXaxis()->SetTitleSize(0.05);
+  gr_err->GetHistogram()->GetXaxis()->SetTitleOffset(1);
+  gr_err->GetHistogram()->GetXaxis()->SetLabelSize(0.05);
+  gr_err->GetHistogram()->GetYaxis()->SetTitleSize(0.05);
+  gr_err->GetHistogram()->GetYaxis()->SetTitleOffset(1);
+  gr_err->GetHistogram()->GetYaxis()->SetLabelSize(0.05);
+  
   gr_err->Draw("a4");
-  C1->cd(1)->BuildLegend();
 
   TFile* file2 = TFile::Open("ref/tree.root");
   TTree* tree = (TTree*)file2->Get("myTTree");
   TGraph* h = new TGraph();
+  h->SetNameTitle("Reference", "Reference");
   tree->SetLineColor(1);
-  string command = param + "/1000.:T/12>>h";
-  tree->Draw(command.c_str(), "", "L same");
+  tree->SetMarkerStyle(20);
+  tree->SetMarkerSize(0.1);
+  string command = param + "/1000.:T/12>>Reference";
+  tree->Draw(command.c_str(), "", "same");
+  C1->BuildLegend();
 
+  TCanvas* C2 = new TCanvas();
   TGraph* gr_full = GetOneSigmaGraph("systematic/old/tree.root", param, 300);
   gr_full->GetXaxis()->SetRangeUser(0, time_max);
   gr_full->GetYaxis()->SetRangeUser(0, err_max[param]);
   gr_full->SetLineColor(1);
   gr_full->SetTitle("Full Systematic Uncertainty");
-  C1->cd(2);
+  gr_full->GetHistogram()->GetXaxis()->SetTitle("Time [y]");
+  gr_full->GetHistogram()->GetYaxis()->SetTitle("Relative Error Contribution [n.a.]");
+  gr_full->GetHistogram()->GetXaxis()->SetTitleSize(0.05);
+  gr_full->GetHistogram()->GetXaxis()->SetTitleOffset(1);
+  gr_full->GetHistogram()->GetXaxis()->SetLabelSize(0.05);
+  gr_full->GetHistogram()->GetYaxis()->SetTitleSize(0.05);
+  gr_full->GetHistogram()->GetYaxis()->SetTitleOffset(1);
+  gr_full->GetHistogram()->GetYaxis()->SetLabelSize(0.05);
+  C2->cd();
   gr_full->Draw("AL");
 
   vector<TGraph*> v_gr;
   for (int i = 0; i < 5; i++) {
-    v_gr.push_back(add_rel_uncertainty(file[i], param, C1, color[i]));
+    v_gr.push_back(add_rel_uncertainty(file[i], param, C2, color[i]));
   }
-  C1->cd(2)->BuildLegend();
+  C2->BuildLegend();
 }
 
 
 void nice_plot() {
   p_max["P"] = 100;
   err_max["P"] = 0.1;
+  legend["P"] = "Generated Power [GW]";
 
   p_max["B6"] = 1200;
   err_max["B6"] = 0.6;
+  legend["B6"] = "Fissile Inventory [t]";
+  
   p_max["B2"] = 12000;
   err_max["B2"] = 1;
-  p_max["B2_cumul"] = 12000;
-  err_max["B2_cumul"] = 1;
+  
+  p_max["B1_cumul"] = 500000;
+  err_max["B1_cumul"] = 0.2;
+  legend["B1_cumul"] = "U-Nat. Consumption (cumul.) [t]";
 
   my_plot("P");
   my_plot("B6");
-  my_plot("B2_cumul");
+  my_plot("B1_cumul");
 }
